851f8b9643315d88d09d1dc195fbc88e42a2316a
Mirto Rande <mirtorande@gmail.com>
2021-04-17 15:52:51 +0200

    Added table to show guesses at timesteps

--- graphs.py
+++ graphs.py
@@ -4,16 +4,47 @@ import numpy as np
 import pandas as pd
 
 class Graphs:
-    def __init__(self, entropy):
+    def __init__(self, entropy, entropy2, guess, metric_error):
+        # prepare data
+        df = pd.DataFrame({"entropy": entropy[0], "entropy2": entropy2[0], "guess": guess[0], "metric_error": metric_error[0]})
+        #print(df)
 
-        self.fig = plt.figure(frameon=False, figsize=(6, 4))
-        self.ax = self.fig.add_subplot(111)
-        entropy_series = pd.Series(entropy)
-        x_axis = np.arange(0, len(entropy)-1, 1)
-        index_pos = [entropy_series.index.get_loc(y) for y in x_axis]
-        entropy_series.plot.bar()
-        plt.xticks(index_pos, x_axis)
-        plt.title("Enthropy trend")
+        # prepare figure
+        self.fig, self.axs = plt.subplots(2, 3, figsize=(10, 8)) #plt.figure(frameon=False, figsize=(8, 4))
+        # 1
+        x_axis = np.arange(0, len(df["entropy"]), 1)
+        y_axis = df["entropy"]
+        self.axs[0,0].bar(x = x_axis, height = y_axis)
+        self.axs[0,0].set_xticks(x_axis)
+        self.axs[0,0].set_title("Enthropy (nor. minmax)")
+
+        # 2
+        x_axis = np.arange(0, len(df["entropy2"]), 1)
+        y_axis = df["entropy2"]
+        self.axs[0,1].bar(x = x_axis, height = y_axis)
+        self.axs[0,1].set_xticks(x_axis)
+        self.axs[0,1].set_title("Enthropy (nor. sum)")
+
+        # 3
+        guesses = list(enumerate(df["guess"]))
+        columns = ("Timestep", "Goal")
+        #print(guesses)
+        self.axs[0,2].table(cellText = guesses,
+                            colLabels = columns,
+                            loc='best'
+                            )
+        self.axs[0,2].set_title("Goal guesses")
+
+        # 4
+        x_axis = np.arange(0, len(df["metric_error"]), 1)
+        y_axis = df["metric_error"]
+        self.axs[1,0].bar(x = x_axis, height = y_axis)
+        self.axs[1,0].set_xticks(x_axis)
+        self.axs[1,0].set_title("Metric error")
+
+        self.axs[0,2].axis('off')
+        self.axs[1,2].axis('off')
+        self.axs[1,1].axis('off')
 
     def save(self, file_name):
         plt.savefig(
